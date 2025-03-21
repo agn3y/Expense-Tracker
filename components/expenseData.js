f2b3e523e35eb0857d449260aaec865128ed01af
agn3y <agneypatel96@gmail.com>
2025-03-21 17:52:55 +0530

    push my luck

--- components/expenseData.js
+++ components/expenseData.js
@@ -3,42 +3,42 @@ export default [
         id: crypto.randomUUID(),
         title: "Milk",
         category: "Grocery",
-        amount: 0
+        amount: 100
     },
     {
         id: crypto.randomUUID(),
         title: "Shirt",
         category: "Clothes",
-        amount: 0
+        amount: 450
     },
     {
         id: crypto.randomUUID(),
         title: "Electricity Bill",
         category: "Bills",
-        amount: 0
+        amount: 1150
     },
     {
         id: crypto.randomUUID(),
         title: "Keyboard",
         category: "Electronics",
-        amount: 0
+        amount: 500
     },
     {
         id: crypto.randomUUID(),
         title: "Mouse",
         category: "Electronics",
-        amount: 0
+        amount: 500
     },
     {
         id: crypto.randomUUID(),
         title: "C++ for dummies",
         category: "Education",
-        amount: 50
+        amount: 500
     },
     {
         id: crypto.randomUUID(),
         title: "Paracetamol",
         category: "Medicine",
-        amount: 0
+        amount: 100
     }
 ]
